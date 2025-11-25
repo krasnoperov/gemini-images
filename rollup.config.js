eddbc5c4fb5057b262745a23c21229945aad3dac
Aleksei Krasnoperov <aleksei@krasnoperov.me>
2025-11-25 01:14:36 +0100

    Bundle the cli script

--- rollup.config.js
+++ rollup.config.js
@@ -3,55 +3,27 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 
-export default [
-  // Library build (bundled)
-  {
-    input: 'src/index.ts',
-    output: {
-      file: 'dist/index.js',
-      format: 'esm',
-      sourcemap: false,
-    },
-    plugins: [
-      resolve({
-        preferBuiltins: true,
-        browser: false,
-      }),
-      commonjs(),
-      json(),
-      typescript({
-        tsconfig: './tsconfig.json',
-        declaration: true,
-        declarationDir: './dist',
-      }),
-    ],
-    external: [
-      /^node:/,
-    ],
+export default {
+  input: 'src/cli.ts',
+  output: {
+    file: 'dist/cli.js',
+    format: 'esm',
+    sourcemap: false,
+    banner: '#!/usr/bin/env node',
   },
-  // CLI build (bundled)
-  {
-    input: 'src/cli.ts',
-    output: {
-      file: 'dist/cli.js',
-      format: 'esm',
-      sourcemap: false,
-      banner: '#!/usr/bin/env node',
-    },
-    plugins: [
-      resolve({
-        preferBuiltins: true,
-        browser: false,
-      }),
-      commonjs(),
-      json(),
-      typescript({
-        tsconfig: './tsconfig.json',
-        declaration: false,
-      }),
-    ],
-    external: [
-      /^node:/,
-    ],
-  },
-]
+  plugins: [
+    resolve({
+      preferBuiltins: true,
+      browser: false,
+    }),
+    commonjs(),
+    json(),
+    typescript({
+      tsconfig: './tsconfig.json',
+      declaration: false,
+    }),
+  ],
+  external: [
+    /^node:/,
+  ],
+}
